e5b5f6d3302d5558a3d8ca8dc56be01571d988ff
Akash Akter <AkashAkter>
2022-10-24 21:38:14 +0600

    Starting this commit now

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBtKTzTrFXztvKkU1XDEpbuonzmDe9V55M",
+    authDomain: "tutor-hunt-5d565.firebaseapp.com",
+    projectId: "tutor-hunt-5d565",
+    storageBucket: "tutor-hunt-5d565.appspot.com",
+    messagingSenderId: "432326807918",
+    appId: "1:432326807918:web:64c3cfc5a65fd6b80e717f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
